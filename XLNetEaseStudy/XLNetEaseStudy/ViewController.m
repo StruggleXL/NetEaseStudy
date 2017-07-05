f6ef1dcafaf2a3c2ad572ad4069c1d4a129f27b5
xulong <long.xu@srgroup.cn>
2017-07-05 17:20:34 +0800

    初始化

---
+++ XLNetEaseStudy/XLNetEaseStudy/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XLNetEaseStudy
+//
+//  Created by xl on 17/7/5.
+//  Copyright © 2017年 xl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
